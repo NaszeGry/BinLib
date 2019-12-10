cc5430f99fc90bb73bdd814e5629bdfea3195ae3
NaszeGry <52376269+NaszeGry@users.noreply.github.com>
2019-12-10 22:21:45 +0100

    Update CBinLib.h

--- CBinLib.h
+++ CBinLib.h
@@ -2,33 +2,33 @@
 #include <stdbool.h>
 
 #define bit bool
-static bit AND(bit a, bit b) {
+bit AND(bit a, bit b) {
 	return a && b;
 }
 
-static bit NAND(bit a, bit b) {
+bit NAND(bit a, bit b) {
 	return !AND(a, b);
 }
 
 
 // OR
-static bit OR(bit a, bit b) {
+bit OR(bit a, bit b) {
 	return a || b;
 }
 
-static bit NOR(bit a, bit b) {
+bit NOR(bit a, bit b) {
 	return !OR(a, b);
 }
 
-static bit XOR(bit a, bit b) {
+bit XOR(bit a, bit b) {
 	return OR(a, b) && !AND(a, b);
 }
 
-static bit XNOR(bit a, bit b) {
+bit XNOR(bit a, bit b) {
 	return !(OR(a, b) && !AND(a, b));
 }
 
 // not
-static bit NOT(bit a) {
+bit NOT(bit a) {
 	return !a;
 }
